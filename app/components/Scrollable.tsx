182e819b4989d1c6199eacbcab2f157b9ca52fb6
ribhavsharma <ribhavsharma2003@gmail.com>
2024-06-25 05:34:28 +0530

    fix?

--- app/components/Scrollable.tsx
+++ app/components/Scrollable.tsx
@@ -1,104 +1,78 @@
-// components/ScrollableText.tsx
 "use client";
 import { useEffect } from "react";
 
 const ScrollableText = () => {
   useEffect(() => {
-    const textElements = document.querySelectorAll(
-      ".scroll-text",
-    ) as NodeListOf<HTMLElement>;
-    textElements.forEach((textElement) => {
+    const textElements = document.querySelectorAll('.scroll-text') as NodeListOf<HTMLElement>;
+    textElements.forEach(textElement => {
       const text = textElement.innerText;
-      const words = text.split(" ");
-      textElement.innerHTML = "";
+      const words = text.split(' ');
+      textElement.innerHTML = '';
 
-      words.forEach((word) => {
-        const wordElement = document.createElement("span");
-        wordElement.className = "word inline-block";
+      words.forEach(word => {
+        const wordElement = document.createElement('span');
+        wordElement.className = 'word inline-block';
 
         if (word.includes("Ribhav")) {
+          // Additional styles or functionality can be added here if needed
         }
 
-        if (
-          word == "Fullstack" ||
-          word == "Developer" ||
-          word == "UI/UX" ||
-          word == "Designer"
-        ) {
+        if (word === "Fullstack" || word === "Developer" || word === "UI/UX" || word === "Designer") {
           wordElement.classList.add("squiggle");
         }
 
-        word.split("").forEach((char) => {
-          const span = document.createElement("span");
-          span.className =
-            "char inline-block transition-opacity duration-50 ease-in-out";
-          if (char === " ") {
-            span.innerHTML = "&nbsp;";
-          } else if (char === "'") {
-            span.innerHTML = "&apos;";
+        word.split('').forEach(char => {
+          const span = document.createElement('span');
+          span.className = 'char inline-block transition-opacity duration-50 ease-in-out';
+          if (char === ' ') {
+            span.innerHTML = '&nbsp;';
           } else {
             span.innerText = char;
           }
-          span.style.color = "rgba(0, 0, 0, 0.2)";
+          span.style.color = 'rgba(0, 0, 0, 0.2)'; // Initially light grey
 
           wordElement.appendChild(span);
         });
 
         textElement.appendChild(wordElement);
 
-        const spaceSpan = document.createElement("span");
-        spaceSpan.innerText = " ";
-        textElement.appendChild(spaceSpan);
+        const spaceSpan = document.createElement('span');
+        spaceSpan.innerText = ' ';
+        textElement.appendChild(spaceSpan); // Add space between words
       });
     });
 
     const handleScroll = () => {
-      const spans = document.querySelectorAll(
-        ".char",
-      ) as NodeListOf<HTMLElement>;
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
+      if (window.innerWidth > 768) {
+        const spans = document.querySelectorAll('.char') as NodeListOf<HTMLElement>;
+        const scrollPosition = window.scrollY;
+        const windowHeight = window.innerHeight;
 
-      spans.forEach((span, index) => {
-        const baseFactor = window.innerWidth <= 768 ? 10 : 2.5;
-        const opacity = Math.min(
-          Math.max(
-            (scrollPosition - index * baseFactor) / (windowHeight / 16),
-            0.2,
-          ),
-          0.8,
-        );
-        span.style.color = `rgba(20,20,20, ${opacity})`;
-      });
+        spans.forEach((span, index) => {
+          const opacity = Math.min(Math.max((scrollPosition - index * 2.5) / (windowHeight / 16), 0.2), 0.8);
+          span.style.color = `rgba(0, 0, 0, ${opacity})`;
+        });
+      }
     };
 
-    document.addEventListener("scroll", handleScroll);
+    document.addEventListener('scroll', handleScroll);
     return () => {
-      document.removeEventListener("scroll", handleScroll);
+      document.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
   return (
     <div className="flex items-start justify-center mt-40 md:mt-16">
-      <div
-        className={`font-rebond text-2xl md:text-4xl ${window.innerWidth > 768 ? "text-gray-200" : "text-[#141414]"} w-[60%]`}
-      >
-        <p className={`${window.innerWidth > 768 ? "scroll-text" : ""}`}>
-          Hi, I&apos;m Ribhav. I am a Fullstack Developer and UI/UX Designer based
-          out of Kelowna, Canada.
-        </p>
-        <br></br>
-
-        <p className={`${window.innerWidth > 768 ? "scroll-text" : ""}`}>
-          I am a Fourth Year CS student at the University of British Columbia,
-          Okanagan. My passion lies in creating intuitive and visually appealing
-          digital experiences. With a strong foundation in both front-end and
-          back-end development, I thrive on bringing ideas to life through code
-          and design.
-        </p>
-        <br></br>
-        <p className={`${window.innerWidth > 768 ? "scroll-text" : ""}`}>
-          Let&apos;s build something splendid together.
+      <div className="font-rebond text-2xl md:text-4xl text-gray-200 w-[90%] sm:w-[80%] md:w-[60%]">
+        <p className="scroll-text">
+          Hi, I am Ribhav. I am a Fullstack Developer and UI/UX Designer based out of Kelowna, Canada.
+        </p><br></br>
+        
+        <p className="scroll-text">
+          I am a Fourth Year CS student at the University of British Columbia, Okanagan. My passion lies in creating intuitive and visually appealing digital experiences. With a strong foundation in both front-end and back-end development, I thrive on bringing ideas to life through code and design.
+        </p><br></br>
+        <p className="scroll-text">
+          Let's build something splendid together.
         </p>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,40 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Navbar from "./components/Navbar";
 import MouseGallery from "./components/MouseGallery";
-import Scrollable from "./components/Scrollable"
+import Scrollable from "./components/Scrollable";
 import Skills from "./components/Skills";
-import locomotiveScroll from 'locomotive-scroll';
-import 'react-tooltip/dist/react-tooltip.css'
+import 'react-tooltip/dist/react-tooltip.css';
 import Separator from "./components/Separator";
 import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer";
 
-
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsClient(true);
+    }
+  }, []);
 
   return (
     <div>
       <Navbar />
       <main>
-      
-          <>
-          {window.innerWidth > 768 && (
-            <MouseGallery />
-          )}
-            <Scrollable />
-            <Separator />
-            <Skills />
-            <Separator />
-            <Projects />
-            <Separator />
-            <ContactMe />
-            <Separator />
-          </>
-        
-        <Footer></Footer>
+        <>
+          {isClient && window.innerWidth > 768 && <MouseGallery />}
+          <Scrollable />
+          <Separator />
+          <Skills />
+          <Separator />
+          <Projects />
+          <Separator />
+          <ContactMe />
+          <Separator />
+        </>
+        <Footer />
       </main>
     </div>
   );
