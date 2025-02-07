67736a5bf2af4c7fc5789936f0c3874b679edb3f
ribhavsharma <ribhavsharma2003@gmail.com>
2025-02-06 22:44:12 -0800

    build fix

--- app/components/SkButton.tsx
+++ app/components/SkButton.tsx
@@ -1,21 +1,31 @@
 "use client"
+
 import type React from "react"
+import { use, useEffect, useState } from "react"
 
 interface Props {
   children: React.ReactNode
   onClick?: () => void
 }
 
-const down = new Audio("/audio/down.mp3")
-const up = new Audio("/audio/up.mp3")
 
 export default function SkButton({ children, onClick }: Props) {
+  const [up, setUpAudio] = useState<HTMLAudioElement | null>(null)
+  const [down, setDownAudio] = useState<HTMLAudioElement | null>(null)
+
+  useEffect(() => {
+    setUpAudio(new Audio("/audio/up.mp3"))
+    setDownAudio(new Audio("/audio/down.mp3"))
+  }, [])
+  
   const handleMouseDown = () => {
+    if (!down) return
     down.currentTime = 0.020
     down.play()
   }
 
   const handleMouseUp = () => {
+    if (!up) return
     up.currentTime = 0.03
     up.play()
   }
